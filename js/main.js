e91e51c364e8763669e2dc3246205b9580566461
Ayeza <ayezabashir442@gmail.com>
2023-12-13 19:28:07 +0500

    menu bar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const listItems = document.querySelectorAll('.menu-list-items');
+
+listItems.forEach(item => {
+    item.addEventListener('click', function () {
+        listItems.forEach(item => {
+            item.classList.remove('font-bold');
+        });
+
+        this.classList.add('font-bold');
+    });
+});
